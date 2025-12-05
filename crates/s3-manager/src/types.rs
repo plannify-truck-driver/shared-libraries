79a829bfd0d064bbe5f6317e1ed86c30d5d35376
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-12-05 22:08:19 +0100

    feat: add DriverMailAttachment to ObjectType enum

--- crates/s3-manager/src/types.rs
+++ crates/s3-manager/src/types.rs
@@ -6,13 +6,22 @@ pub struct ObjectMetadata {
 }
 
 pub enum ObjectType {
-    DriverMonthlyWorkdayReport
+    DriverMonthlyWorkdayReport,
+    DriverMailAttachment,
 }
 
 impl ObjectType {
     pub fn to_s3_path(&self, entity_id: &str) -> String {
         match self {
             ObjectType::DriverMonthlyWorkdayReport => format!("drivers/{}/monthly_workday_reports", entity_id),
+            ObjectType::DriverMailAttachment => format!("drivers/{}/mail_attachments", entity_id),
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        match self {
+            ObjectType::DriverMonthlyWorkdayReport => "DRIVER_MONTHLY_WORKDAY_REPORT".to_string(),
+            ObjectType::DriverMailAttachment => "DRIVER_MAIL_ATTACHMENT".to_string(),
         }
     }
 }
