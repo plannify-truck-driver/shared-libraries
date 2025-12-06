a1caeae4084673a252302b0307df3c17792784a1
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-12-06 11:31:30 +0100

    feat: add endpoint

--- crates/s3-manager/src/config.rs
+++ crates/s3-manager/src/config.rs
@@ -1,5 +1,6 @@
 #[derive(Debug, Clone)]
 pub struct S3Config {
+    pub endpoint: String,
     pub bucket_name: String,
     pub region: String,
     pub access_key: Option<String>,
@@ -9,6 +10,7 @@ pub struct S3Config {
 impl S3Config {
     pub fn default(bucket: impl Into<String>, region: impl Into<String>) -> Self {
         Self {
+            endpoint: String::new(),
             bucket_name: bucket.into(),
             region: region.into(),
             access_key: None,
